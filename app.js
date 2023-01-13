66d6db0014d403982f47ea7934e235167e9137fd
Vishz07 <anandnairaok@gmail.com>
2023-01-13 10:59:27 +0530

    updated js

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const billAmount = document.getElementById("bill-Amount");
+const billAmount = document.querySelector("#bill-Amount")
 const cashGiven = document.getElementById("cash-Given");
 const btn = document.getElementById("btn");
 const errorMessage = document.getElementById("error-Message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
+
+const availableNotes = [2000 ,500 , 100, 20, 10 , 5, 1]
 
 btn.addEventListener("click", function ValidCashGiven(){
+  console.log(billAmount.value)
   errorMessage.style.display="none";
   if (billAmount.value > 0 ){
-    if (billAmount.value >= cashGiven.value){
-
+      if (cashGiven.value >= billAmount.value){
+      
+          const amountToBeReturned = cashGiven.value - billAmount.value;
+          calculateChange(amountToBeReturned)
+          
     }else{
-
+      showMessage("you need to give more cash")
     }
   }else{
-    
+    showMessage("Bill amount should be more than 0")
   }
 });
 
 
-function showMessage(){
+function calculateChange(amountToBeReturned){
+  for(let i =0; i< availableNotes.length; i++){
+    const numberofNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+    amountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerHTML = numberofNotes;
+  }
+}
+
+function showMessage(errormsg){
   errorMessage.style.display="block";
-    errorMessage.innerText = "the bill amount should be grater than 0";
+    errorMessage.innerText = errormsg;
 }
 
